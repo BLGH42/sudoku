b502991b7e48b32e881b440662961c76713e0b67
bleotard <bleotard@gmail.com>
2020-11-11 15:04:11 +0100

    first commit

---
+++ inc/sudoku.h
@@ -0,0 +1,11 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+#include<stdio.h>
+#include<unistd.h>
+
+void input_grid(char s[9][9]);
+void print_grid_start(char s[9][9]);
+void print_solved_grid(char s[9][9]);
+
+#endif
---
+++ src/grid.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"sudoku.h"
+
+void input_grid(char s[9][9])
+{
+	int i;
+	int j;
+	printf("Enter a 9x9 grid with digits from 1 to 9 in one single line, from top left to bottom right following the lines.\nNo identical digits in the same line, column or 3x3 subsquare.\nType . for empty tile.\n");
+	i = 0;
+	while(i < 9)
+	{
+		j = 0;
+		while(j < 9)
+		{
+			scanf("%c", &s[i][j]);
+			if(s[i][j] == '.' || (s[i][j] >= '1' && s[i][j] <= '9'))
+				;
+			else
+			{
+			    printf("Instructions not followed.\n");
+				exit(0);
+			}
+			++j;
+		}
+		++i;
+	}
+	return;
+}
+
+void print_grid_start(char s[9][9])
+{
+	int i;
+	int j;
+	
+	i = 0;
+	while(i < 9)
+	{
+		printf("\n");
+		j = 0;
+		while(j < 9)
+		{
+			printf("%c", s[i][j]);
+			++j;
+		}
+		++i;
+	}
+	printf("\n");
+	return;
+}
+
+void print_solved_grid(char s[9][9])
+{
+	int i;
+	int j;
+
+	i = 0;
+	while(i < 9)
+	{
+		printf("\n");
+		j = 0;
+		while(j < 9)
+		{
+			printf("%c", s[i][j]);
+			++j;
+		}
+		++i;
+	}
+	printf("\n");
+	return;
+}
