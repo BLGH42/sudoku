ac13befad545cbd394eca3573be2cdb17f4bcea3
bleotard <bleotard@gmail.com>
2020-11-19 16:42:13 +0100

    clean loops

--- src/solver.c
+++ src/solver.c
@@ -66,52 +66,28 @@ int 	main()
 				j = 0;
 				while (j < 9)
 				{
-					if (0 <= i && i < 3 && 0 <= j && j < 3)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_top_left(p, s, i, j, q);
-					}
-					else if (0 <= i && i < 3 && 3 <= j && j < 6)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_top_mid(p, s, i, j, q);
-					}
-					else if (0 <= i && i < 3 && 6 <= j && j < 9)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_top_right(p, s, i, j, q);
-					}
-					else if (3 <= i && i < 6 && 0 <= j && j < 3)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_mid_left(p, s, i, j, q);
-					}
-					else if (3 <= i && i < 6 && 3 <= j && j < 6)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_mid_mid(p, s, i, j, q);
-					}
-					else if (3 <= i && i < 6 && 6 <= j && j < 9)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_mid_right(p, s, i, j, q);
-					}
-					else if (6 <= i && i < 9 && 0 <= j && j < 3)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_bot_left(p, s, i, j, q);
-					}
-					else if(6 <= i && i < 9 && 3 <= j && j < 6)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_bot_mid(p, s, i, j, q);
-					}
-					else if(6 <= i && i < 9 && 6 <= j && j < 9)
-					{
-						if(p[i][j][q] == '.')
-							square_finder_bot_right(p, s, i, j, q);
-					}
-					++j;
+						if (p[i][j][q] == '.')
+						{
+							if (0 <= i && i < 3 && 0 <= j && j < 3)
+								square_finder_top_left(p, s, i, j, q);
+							else if (0 <= i && i < 3 && 3 <= j && j < 6)
+								square_finder_top_mid(p, s, i, j, q);
+							else if (0 <= i && i < 3 && 6 <= j && j < 9)
+								square_finder_top_right(p, s, i, j, q);
+							else if (3 <= i && i < 6 && 0 <= j && j < 3)
+								square_finder_mid_left(p, s, i, j, q);
+							else if (3 <= i && i < 6 && 3 <= j && j < 6)
+								square_finder_mid_mid(p, s, i, j, q);
+							else if (3 <= i && i < 6 && 6 <= j && j < 9)
+								square_finder_mid_right(p, s, i, j, q);
+							else if (6 <= i && i < 9 && 0 <= j && j < 3)
+								square_finder_bot_left(p, s, i, j, q);
+							else if(6 <= i && i < 9 && 3 <= j && j < 6)
+								square_finder_bot_mid(p, s, i, j, q);
+							else if(6 <= i && i < 9 && 6 <= j && j < 9)
+								square_finder_bot_right(p, s, i, j, q);
+						}
+						++j;
 				}
 				++i;
 			}
@@ -161,28 +137,28 @@ void 	line_finder(char p[9][9][9], char s[9][9], int i, int j)
 	int		q;
 
 	if(s[i][j] == '.' || s[i][j] == '+')
+	{
+		q = 1;
+		while (q <= 9)
 		{
-			q = 1;
-			while (q <= 9)
+			{
+				s[i][j] = q + '0';
+				k = 0;
+				while(k < 9)
 				{
-					{
-						s[i][j] = q + '0';
-						k = 0;
-						while(k < 9)
-							{
-								if(s[i][k] != '.' && s[i][k] != '+')
-									if(k != j && s[i][k] == s[i][j])
-										p[i][j][q] = 'X';
-								if(s[k][j] != '.' && s[k][j] != '+')
-									if(k != i && s[k][j] == s[i][j])
-										p[i][j][q] = 'X';
-								++k;
-							}
-					}
-					++q;
+					if(s[i][k] != '.' && s[i][k] != '+')
+						if(k != j && s[i][k] == s[i][j])
+							p[i][j][q] = 'X';
+					if(s[k][j] != '.' && s[k][j] != '+')
+						if(k != i && s[k][j] == s[i][j])
+							p[i][j][q] = 'X';
+					++k;
 				}
-			s[i][j] = '.';
+			}
+			++q;
 		}
+		s[i][j] = '.';
+	}
 	return ;
 }
 
@@ -195,7 +171,7 @@ void 	square_finder_top_left(char p[9][9][9], char s[9][9], int i, int j, int q)
 	{
 		s[i][j] = q + '0';
 		k = 0;
-	    while(k < 3)
+		while(k < 3)
 		{
 			l = 0;
 			while(l < 3)
@@ -224,7 +200,7 @@ void 	square_finder_top_mid(char p[9][9][9], char s[9][9], int i, int j, int q)
 	{
 		s[i][j] = q + '0';
 		k = 0;
-	    while(k < 3)
+		while(k < 3)
 		{
 			l = 3;
 			while(l < 6)
@@ -253,7 +229,7 @@ void 	square_finder_top_right(char p[9][9][9], char s[9][9], int i, int j, int q
 	{
 		s[i][j] = q + '0';
 		k = 0;
-	    while(k < 3)
+		while(k < 3)
 		{
 			l = 6;
 			while(l < 9)
@@ -311,7 +287,7 @@ void 	square_finder_mid_mid(char p[9][9][9], char s[9][9], int i, int j, int q)
 	{
 		s[i][j] = q + '0';
 		k = 3;
-	    while(k < 6)
+		while(k < 6)
 		{
 			l = 3;
 			while(l < 6)
@@ -340,7 +316,7 @@ void 	square_finder_mid_right(char p[9][9][9], char s[9][9], int i, int j, int q
 	{
 		s[i][j] = q + '0';
 		k = 3;
-	    while(k < 6)
+		while(k < 6)
 		{
 			l = 6;
 			while(l < 9)
@@ -369,7 +345,7 @@ void 	square_finder_bot_left(char p[9][9][9], char s[9][9], int i, int j, int q)
 	{
 		s[i][j] = q + '0';
 		k = 6;
-	    while(k < 9)
+		while(k < 9)
 		{
 			l = 0;
 			while(l < 3)
@@ -398,7 +374,7 @@ void 	square_finder_bot_mid(char p[9][9][9], char s[9][9], int i, int j, int q)
 	{
 		s[i][j] = q + '0';
 		k = 6;
-	    while(k < 9)
+		while(k < 9)
 		{
 			l = 3;
 			while(l < 6)
@@ -427,7 +403,7 @@ void 	square_finder_bot_right(char p[9][9][9], char s[9][9], int i, int j, int q
 	{
 		s[i][j] = q + '0';
 		k = 6;
-	    while(k < 9)
+		while(k < 9)
 		{
 			l = 6;
 			while(l < 9)
