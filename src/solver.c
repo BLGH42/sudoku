91c2964a84341d0a843e3a345c86ac8db17e74ea
bleotard <bleotard@gmail.com>
2020-11-19 17:50:55 +0100

    all functions with same parameters

--- src/solver.c
+++ src/solver.c
@@ -2,16 +2,16 @@
 #include<stdlib.h>
 #include "sudoku.h"
 
-void 	line_finder(char p[9][9][9], char all_tiles[9][9], int coordinate1, int coordinate2);
-void 	square_finder_top_left(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_top_mid(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_top_right(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_mid_left(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_mid_mid(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_mid_right(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_bot_left(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_bot_mid(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
-void 	square_finder_bot_right(char p[9][9][9], char s[9][9], int counter1, int counter2, int counter3);
+void 	line_finder(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_top_left(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_top_mid(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_top_right(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_mid_left(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_mid_mid(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_mid_right(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_bot_left(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_bot_mid(char p[9][9][9], char s[9][9], int i, int j);
+void 	square_finder_bot_right(char p[9][9][9], char s[9][9], int i, int j);
 void 	grid_sweep(char p[9][9][9], char s[9][9], void (*f)(char [9][9][9], char[9][9], int, int));
 	
 int 	main()
@@ -69,23 +69,23 @@ int 	main()
 						if (p[i][j][q] == '.')
 						{
 							if (0 <= i && i < 3 && 0 <= j && j < 3)
-								square_finder_top_left(p, s, i, j, q);
+								square_finder_top_left(p, s, i, j);
 							else if (0 <= i && i < 3 && 3 <= j && j < 6)
-								square_finder_top_mid(p, s, i, j, q);
+								square_finder_top_mid(p, s, i, j);
 							else if (0 <= i && i < 3 && 6 <= j && j < 9)
-								square_finder_top_right(p, s, i, j, q);
+								square_finder_top_right(p, s, i, j);
 							else if (3 <= i && i < 6 && 0 <= j && j < 3)
-								square_finder_mid_left(p, s, i, j, q);
+								square_finder_mid_left(p, s, i, j);
 							else if (3 <= i && i < 6 && 3 <= j && j < 6)
-								square_finder_mid_mid(p, s, i, j, q);
+								square_finder_mid_mid(p, s, i, j);
 							else if (3 <= i && i < 6 && 6 <= j && j < 9)
-								square_finder_mid_right(p, s, i, j, q);
+								square_finder_mid_right(p, s, i, j);
 							else if (6 <= i && i < 9 && 0 <= j && j < 3)
-								square_finder_bot_left(p, s, i, j, q);
+								square_finder_bot_left(p, s, i, j);
 							else if(6 <= i && i < 9 && 3 <= j && j < 6)
-								square_finder_bot_mid(p, s, i, j, q);
+								square_finder_bot_mid(p, s, i, j);
 							else if(6 <= i && i < 9 && 6 <= j && j < 9)
-								square_finder_bot_right(p, s, i, j, q);
+								square_finder_bot_right(p, s, i, j);
 						}
 						++j;
 				}
@@ -141,19 +141,17 @@ void 	line_finder(char p[9][9][9], char s[9][9], int i, int j)
 		q = 1;
 		while (q <= 9)
 		{
+			k = 0;
+			while(k < 9)
 			{
 				s[i][j] = q + '0';
-				k = 0;
-				while(k < 9)
-				{
-					if(s[i][k] != '.' && s[i][k] != '+')
-						if(k != j && s[i][k] == s[i][j])
-							p[i][j][q] = 'X';
-					if(s[k][j] != '.' && s[k][j] != '+')
-						if(k != i && s[k][j] == s[i][j])
-							p[i][j][q] = 'X';
-					++k;
-				}
+				if(s[i][k] != '.' && s[i][k] != '+')
+					if(k != j && s[i][k] == s[i][j])
+						p[i][j][q] = 'X';
+				if(s[k][j] != '.' && s[k][j] != '+')
+					if(k != i && s[k][j] == s[i][j])
+						p[i][j][q] = 'X';
+				++k;
 			}
 			++q;
 		}
@@ -162,261 +160,315 @@ void 	line_finder(char p[9][9][9], char s[9][9], int i, int j)
 	return ;
 }
 
-void 	square_finder_top_left(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_top_left(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 0;
-		while(k < 3)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 0;
-			while(l < 3)
+			s[i][j] = q + '0';
+			k = 0;
+			while(k < 3)
 			{
-				if (s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}	
-				++l;
+				l = 0;
+				while(l < 3)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_top_mid(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_top_mid(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 0;
-		while(k < 3)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 3;
-			while(l < 6)
+			s[i][j] = q + '0';
+			k = 0;
+			while(k < 3)
 			{
-				if (s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}	
-				++l;
+				l = 3;
+				while(l < 6)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_top_right(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_top_right(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 0;
-		while(k < 3)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 6;
-			while(l < 9)
+			s[i][j] = q + '0';
+			k = 0;
+			while(k < 3)
 			{
-				if (s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}	
-				++l;
+				l = 6;
+				while(l < 9)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_mid_left(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_mid_left(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 3;
-	    while(k < 6)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 0;
-			while(l < 3)
+			s[i][j] = q + '0';
+			k = 3;
+			while(k < 6)
 			{
-				if(s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}
-				++l;
+				l = 0;
+				while(l < 3)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_mid_mid(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_mid_mid(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 3;
-		while(k < 6)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 3;
-			while(l < 6)
+			s[i][j] = q + '0';
+			k = 3;
+			while(k < 6)
 			{
-				if(s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}
-				++l;
+				l = 3;
+				while(l < 6)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_mid_right(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_mid_right(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 3;
-		while(k < 6)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 6;
-			while(l < 9)
+			s[i][j] = q + '0';
+			k = 3;
+			while(k < 6)
 			{
-				if(s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}
-				++l;
+				l = 6;
+				while(l < 9)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_bot_left(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_bot_left(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 6;
-		while(k < 9)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 0;
-			while(l < 3)
+			s[i][j] = q + '0';
+			k = 6;
+			while(k < 9)
 			{
-				if(s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}
-				++l;
+				l = 0;
+				while(l < 3)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_bot_mid(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_bot_mid(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 6;
-		while(k < 9)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 3;
-			while(l < 6)
+			s[i][j] = q + '0';
+			k = 6;
+			while(k < 9)
 			{
-				if(s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}
-				++l;
+				l = 3;
+				while(l < 6)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
 	return ;
 }
 
-void 	square_finder_bot_right(char p[9][9][9], char s[9][9], int i, int j, int q)
+void 	square_finder_bot_right(char p[9][9][9], char s[9][9], int i, int j)
 {
 	int 	k;
 	int 	l;
+	int		q;
 
 	if(s[i][j] == '.')
 	{
-		s[i][j] = q + '0';
-		k = 6;
-		while(k < 9)
+		q = 1;
+		while (q <= 9)
 		{
-			l = 6;
-			while(l < 9)
+			s[i][j] = q + '0';
+			k = 6;
+			while(k < 9)
 			{
-				if(s[k][l] != '.' && s[k][l] != '+')
-					if(k != i && l != j && s[i][j] == s[k][l])
-					{
-						s[i][j] = '.';
-						p[i][j][q] = 'X';
-					}
-				++l;
+				l = 6;
+				while(l < 9)
+				{
+					if (s[k][l] != '.' && s[k][l] != '+')
+						if(k != i && l != j && s[i][j] == s[k][l])
+						{
+							s[i][j] = '.';
+							p[i][j][q] = 'X';
+						}	
+					++l;
+				}
+				++k;
 			}
-			++k;
+			++q;
 		}
 		s[i][j] = '.';
 	}
